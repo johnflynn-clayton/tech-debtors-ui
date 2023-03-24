3ea427671255a93272669d5eb96ee852e7e2fb04
gfresh33 <garrett.freshwater@claytonhomes.com>
2023-03-24 10:25:30 -0400

    Minor text changes

--- src/App.js
+++ src/App.js
@@ -119,14 +119,14 @@ const App = () => {
             <span class="sr-only">Menu</span>
             <img
                 alt="Man"
-                src="https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                src="https://media.istockphoto.com/id/1308394354/photo/senior-man-using-tablet-to-talk-to-his-family.jpg?s=1024x1024&w=is&k=20&c=nbwYw8GZt3bwh8eZ9qfhPVhxZSC7Ocxceb4xtetDR_g="
                 class="h-10 w-10 rounded-full object-cover"
             />
 
             <p class="ml-2 hidden text-left text-xs sm:block">
-                <strong class="block font-medium">Eric Frusciante</strong>
+                <strong class="block font-medium">Frank Lassiter</strong>
 
-                <span class="text-gray-500"> eric@frusciante.com </span>
+                <span class="text-gray-500"> frank.lassiter@claytonhomes.com </span>
             </p>
 
             <svg
@@ -147,11 +147,11 @@ const App = () => {
 
         <div class="mt-8">
         <h1 class="text-2xl font-bold text-blue-50 sm:text-3xl">
-            Welcome Back, Barry!
+            Welcome Back, Frank!
         </h1>
 
         <p class="mt-1.5 text-sm text-gray-500">
-            Your website has seen a 52% increase in traffic in the last month. Keep
+            Your asset tracker has seen a 85% decrease in theft in the last month. Keep
             it up! 🚀
         </p>
         </div>
@@ -197,90 +197,6 @@ const App = () => {
           </a>
       </div>
     
-      {/* <div class="h-screen py-8 overflow-y-auto bg-white border-l border-r sm:w-64 w-60 bg-gray-900 dark:bg-gray-900 dark:border-gray-700">
-          <h2 class="px-5 text-lg font-medium text-gray-800 dark:text-white">Assets</h2>
-
-          <div class="mt-8 space-y-4">
-              <button class="flex items-center w-full px-5 py-2 transition-colors duration-200 dark:hover:bg-gray-800 gap-x-2 hover:bg-gray-100 focus:outline-none">
-                  <img class="object-cover w-8 h-8 rounded-full" src="https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=faceare&facepad=3&w=688&h=688&q=100" alt="" />
-              
-                  <div class="text-left rtl:text-right">
-                      <h1 class="text-sm font-medium text-gray-700 capitalize dark:text-white">Mia John</h1>
-      
-                      <p class="text-xs text-gray-500 dark:text-gray-400">11.2 Followers</p>
-                  </div>
-              </button>
-
-              <button class="flex items-center w-full px-5 py-2 transition-colors duration-200 dark:hover:bg-gray-800 gap-x-2 hover:bg-gray-100 focus:outline-none">
-                  <img class="object-cover w-8 h-8 rounded-full" src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&h=880&q=80" alt="" />
-              
-                  <div class="text-left rtl:text-right">
-                      <h1 class="text-sm font-medium text-gray-700 capitalize dark:text-white">arthur melo</h1>
-      
-                      <p class="text-xs text-gray-500 dark:text-gray-400">1.2 Followers</p>
-                  </div>
-              </button>
-
-              <button class="flex items-center w-full px-5 py-2 transition-colors duration-200 bg-gray-100 dark:bg-gray-800 gap-x-2 focus:outline-none">
-                  <div class="relative">
-                      <img class="object-cover w-8 h-8 rounded-full" src="https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&h=764&q=100" alt="" />
-                      <span class="h-2 w-2 rounded-full bg-emerald-500 absolute right-0.5 ring-1 ring-white bottom-0"></span>
-                  </div>
-
-                  <div class="text-left rtl:text-right">
-                      <h1 class="text-sm font-medium text-gray-700 capitalize dark:text-white">Jane Doe</h1>
-      
-                      <p class="text-xs text-gray-500 dark:text-gray-400">15.6 Followers</p>
-                  </div>
-              </button>
-
-              <button class="flex items-center w-full px-5 py-2 transition-colors duration-200 dark:hover:bg-gray-800 gap-x-2 hover:bg-gray-100 focus:outline-none">
-                  <img class="object-cover w-8 h-8 rounded-full" src="https://images.unsplash.com/photo-1531590878845-12627191e687?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&h=764&q=80" alt="" />
-              
-                  <div class="text-left rtl:text-right">
-                      <h1 class="text-sm font-medium text-gray-700 capitalize dark:text-white">arthur melo</h1>
-      
-                      <p class="text-xs text-gray-500 dark:text-gray-400">32.9 Followers</p>
-                  </div>
-              </button>
-
-              <button class="flex items-center w-full px-5 py-2 transition-colors duration-200 dark:hover:bg-gray-800 gap-x-2 hover:bg-gray-100 focus:outline-none">
-                  <img class="object-cover w-8 h-8 rounded-full" src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&h=687&q=80" alt="" />
-              
-                  <div class="text-left rtl:text-right">
-                      <h1 class="text-sm font-medium text-gray-700 capitalize dark:text-white">Joseph Gonzalez</h1>
-      
-                      <p class="text-xs text-gray-500 dark:text-gray-400">100.2 Followers</p>
-                  </div>
-              </button>
-
-              <button class="flex items-center w-full px-5 py-2 transition-colors duration-200 hover:bg-gray-100 dark:hover:bg-gray-800 gap-x-2 focus:outline-none">
-                  <div class="relative">
-                      <img class="object-cover w-8 h-8 rounded-full" src="https://images.unsplash.com/photo-1488508872907-592763824245?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&h=1470&q=80" alt="" />
-                      <span class="h-2 w-2 rounded-full bg-emerald-500 absolute right-0.5 ring-1 ring-white bottom-0"></span>
-                  </div>
-
-                  <div class="text-left rtl:text-right">
-                      <h1 class="text-sm font-medium text-gray-700 capitalize dark:text-white">Olivia Wathan</h1>
-      
-                      <p class="text-xs text-gray-500 dark:text-gray-400">8.6 Followers</p>
-                  </div>
-              </button>
-
-              <button class="flex items-center w-full px-5 py-2 transition-colors duration-200 hover:bg-gray-100 dark:hover:bg-gray-800 gap-x-2 focus:outline-none">
-                  <div class="relative">
-                      <img class="object-cover w-8 h-8 rounded-full" src="https://images.unsplash.com/photo-1608174386344-80898cec6beb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&h=687&q=80" alt="" />
-                      <span class="h-2 w-2 rounded-full bg-emerald-500 absolute right-0.5 ring-1 ring-white bottom-0"></span>
-                  </div>
-
-                  <div class="text-left rtl:text-right">
-                      <h1 class="text-sm font-medium text-gray-700 capitalize dark:text-white">Junior REIS</h1>
-      
-                      <p class="text-xs text-gray-500 dark:text-gray-400">56.6 Followers</p>
-                  </div>
-              </button>
-          </div>
-      </div> */}
       <div className="mapContainer bg-gray-900 text-white py-6">
         <div className="container mx-0 px-2 bg-gray-900 flex">
           <MapContainer />
@@ -382,29 +298,6 @@ const App = () => {
               target="_blank"
               class="text-teal-700 transition hover:text-teal-700/75"
             >
-              <span class="sr-only">Instagram</span>
-              <svg
-                class="h-6 w-6"
-                fill="currentColor"
-                viewBox="0 0 24 24"
-                aria-hidden="true"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-            </a>
-          </li>
-
-          <li>
-            <a
-              href="/"
-              rel="noreferrer"
-              target="_blank"
-              class="text-teal-700 transition hover:text-teal-700/75"
-            >
               <span class="sr-only">Twitter</span>
               <svg
                 class="h-6 w-6"
@@ -436,29 +329,6 @@ const App = () => {
                 <path
                   fill-rule="evenodd"
                   d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-            </a>
-          </li>
-
-          <li>
-            <a
-              href="/"
-              rel="noreferrer"
-              target="_blank"
-              class="text-teal-700 transition hover:text-teal-700/75"
-            >
-              <span class="sr-only">Dribbble</span>
-              <svg
-                class="h-6 w-6"
-                fill="currentColor"
-                viewBox="0 0 24 24"
-                aria-hidden="true"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10c5.51 0 10-4.48 10-10S17.51 2 12 2zm6.605 4.61a8.502 8.502 0 011.93 5.314c-.281-.054-3.101-.629-5.943-.271-.065-.141-.12-.293-.184-.445a25.416 25.416 0 00-.564-1.236c3.145-1.28 4.577-3.124 4.761-3.362zM12 3.475c2.17 0 4.154.813 5.662 2.148-.152.216-1.443 1.941-4.48 3.08-1.399-2.57-2.95-4.675-3.189-5A8.687 8.687 0 0112 3.475zm-3.633.803a53.896 53.896 0 013.167 4.935c-3.992 1.063-7.517 1.04-7.896 1.04a8.581 8.581 0 014.729-5.975zM3.453 12.01v-.26c.37.01 4.512.065 8.775-1.215.25.477.477.965.694 1.453-.109.033-.228.065-.336.098-4.404 1.42-6.747 5.303-6.942 5.629a8.522 8.522 0 01-2.19-5.705zM12 20.547a8.482 8.482 0 01-5.239-1.8c.152-.315 1.888-3.656 6.703-5.337.022-.01.033-.01.054-.022a35.318 35.318 0 011.823 6.475 8.4 8.4 0 01-3.341.684zm4.761-1.465c-.086-.52-.542-3.015-1.659-6.084 2.679-.423 5.022.271 5.314.369a8.468 8.468 0 01-3.655 5.715z"
                   clip-rule="evenodd"
                 />
               </svg>
@@ -524,7 +394,7 @@ const App = () => {
                   class="text-gray-700 transition hover:text-gray-700/75"
                   href="/"
                 >
-                  Web Development
+                  Asset Development
                 </a>
               </li>
 
@@ -533,7 +403,7 @@ const App = () => {
                   class="text-gray-700 transition hover:text-gray-700/75"
                   href="/"
                 >
-                  Web Design
+                  Asset Sales
                 </a>
               </li>
 
@@ -630,7 +500,7 @@ const App = () => {
                   />
                 </svg>
 
-                <span class="text-gray-700">john@doe.com</span>
+                <span class="text-gray-700">frank.lassiter@claytonhomes.com</span>
               </a>
             </li>
 
@@ -654,7 +524,7 @@ const App = () => {
                   />
                 </svg>
 
-                <span class="text-gray-700">0123456789</span>
+                <span class="text-gray-700">5551112222</span>
               </a>
             </li>
 
@@ -682,7 +552,7 @@ const App = () => {
               </svg>
 
               <address class="-mt-0.5 not-italic text-gray-700">
-                213 Lane, London, United Kingdom
+                5000 Clayon Road, Maryville TN
               </address>
             </li>
           </ul>
@@ -713,7 +583,7 @@ const App = () => {
         </p>
 
         <p class="mt-4 text-sm text-gray-500 sm:order-first sm:mt-0">
-          &copy; 2022 Company Name
+          &copy; 2022 Clayton Homes
         </p>
       </div>
     </div>
